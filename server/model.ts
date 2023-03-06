b7f87d7fbc786c67f142bf643c7d4b4357f054b3
Nathanael <nathanaeltjen@hotmail.com>
2023-03-05 23:56:50 -0800

    completed endpoint March 5,23

--- server/model.ts
+++ server/model.ts
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const uri = 'mongodb://localhost:27017/planny';
 mongoose.connect(uri);
-const db = mongoose.connection;
+const dbs = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function () {
+dbs.on('error', console.error.bind(console, 'connection error:'))
+dbs.once('open', function () {
   console.log('database connected')
 })
 
@@ -34,7 +34,8 @@ let userSchema = mongoose.Schema({
 });
 
 let householdSchema = mongoose.Schema({
-  houseHoldId:{type:Number},
+  householdId:{type:Number, required: true},
+  householdName:{type:String, required: true},
   members:[{type:String}], //array of member's id
   plants:[{type:Number}], // array of plant's id
   messageId:{type:Number}
@@ -42,25 +43,32 @@ let householdSchema = mongoose.Schema({
 
 let messagesSchema = mongoose.Schema({
   messageId: {type:Number},
+  to:{type:String},
   messages:[{
     count:{
       type:Number,
       required: true
     },
-    user:{
+    userId:{
       type:String,
       required:true
     },
     time:{
       type: Date,
       required: true
-    }}],
+    },
     message: {
-      type: String
+      type: String,
+      required:true
     }
+  }],
 })
 
 let plantSchema = mongoose.Schema({
+  userId:{
+    type:String,
+    required:true
+  },
   plantId:{
     type:Number,
     required:true
@@ -92,6 +100,17 @@ let plantSchema = mongoose.Schema({
   photo:{type:String}
 })
 
+let communitySchema = mongoose.Schema({
+  communityId: {type: Number, required: true},
+  time:{type:Date, required: true},
+  messageId:{type:Number},
+  photos:[{type:String}],
+  userId: {type:String, required: true},
+  topic:{type:String, required: true},
+  plantName:{type:String},
+  plantType:{type:String}
+})
+
 
 
 
@@ -101,6 +120,7 @@ let User = mongoose.model('User', userSchema);
 let Household = mongoose.model('Household', householdSchema)
 let Messages = mongoose.model('Messages', messagesSchema)
 let Plant = mongoose.model('Plants', plantSchema)
+let Community = mongoose.model('Community', communitySchema)
 
 
 
@@ -108,5 +128,6 @@ module.exports = {
   User: User,
   Household: Household,
   Messages: Messages,
-  Plant: Plant
+  Plant: Plant,
+  Community: Community
 }
---
+++ server/router.ts
@@ -0,0 +1,112 @@
+import { removePushTokenSubscription } from "expo-notifications";
+
+const routers = require('express').Router();
+
+const controllers = require('./controllers');
+
+
+routers.post('/user', controllers.createUser)
+// {
+//   "userId": "try1",
+//   "firstName": "tryFN",
+//   "lastName":"tryLN",
+//   "email":"nate@gmail.com"
+// }
+
+routers.get('/user', controllers.findUser)
+routers.post('/plant', controllers.addPlant)
+// {
+//   "userId":"try1",
+//   "plant": {
+//       "plantName":"tryPlant",
+//       "plantType":"tryPlant",
+//       "location":"tryPlant",
+//       "careInstructions":"tryPlant",
+//       "wateringSchedule":5
+//   }
+// }
+
+routers.get('/plant', controllers.findPlant)
+// it can be userId:, or plantId:
+
+routers.post('/dm', controllers.postDm)
+// {
+// "from": (userId),
+// "to": (userId),
+// "message":{ string}
+// "time":{date}
+// }
+
+routers.put('/message', controllers.updateMessage)
+// {
+//   messageId:****,
+//   message:{
+//     user: userId,
+//     time: new Date (),
+//     message:String
+//   },
+// }
+
+routers.get('/message', controllers.findMessage)
+// in params
+// {
+//   "messageId":1
+// }
+routers.get('/household', controllers.findHousehold)
+// in params
+// {
+//   "householdId": 1
+// }
+routers.post('/household', controllers.makeHousehold)
+// {
+//   "userId":"try1",
+//   "household": {
+//     "householdName":"try1-house"
+//  }
+// }
+
+routers.put('/household', controllers.updateHousehold)
+//in params
+// {
+//   "householdId": 1,
+//   "userId": "try1"
+// }
+// {
+//   "householdId": 1,
+//   "plants": 1
+// }
+
+routers.post('/householdMessage', controllers.postHouseholdMessage)
+// in body
+// {
+// "householdId": (householdId),
+// "userId": (userId),
+// "message":{ string}
+// "time":{date}
+// }
+
+routers.post('/community', controllers.postCommunity)
+// in body
+// {
+//   "time": "2023-03-06T01:53:53.001Z",
+//   "photos":[],
+//   "userId":"try1",
+//   "topic":"topic1",
+//   "plantName":"optional",
+//   "plantType":"optional"
+// }
+
+routers.post('/communityMessage', controllers.postCommunityMessage)
+// in body
+// {
+// "communityId": (communityId),
+// "userId": (userId),
+// "message":{ string}
+// "time":{date}
+// }
+
+routers.get('/community', controllers.findCommunity)
+// no need params
+
+
+module.exports = routers;
