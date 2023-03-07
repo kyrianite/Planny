a317dee43b992265a7a75d1d556db53c8337298b
kyrianite <jessicavu.od@gmail.com>
2023-03-06 23:36:17 -0800

    linked plant api to add plant

--- src/components/PlantProfile/PlantDataAPI.ts
+++ src/components/PlantProfile/PlantDataAPI.ts
@@ -1,30 +1,28 @@
 // https://perenual.com/docs/api
-import * as dotenv from 'dotenv';
-dotenv.config();
+
 import axios, { AxiosResponse } from 'axios';
 import * as PlantTypes from './PlantTypes';
+import { PERENUAL_API_KEY } from '@env';
 
 const baseURL = 'https://perenual.com/api/';
 const axiosOption = {headers: {'content-type': 'application/json'}};
 
-module.exports = {
-  getPlantList: async (name: string) => {
-    try {
-      const plants = await axios.get(`${baseURL}species-list?q=${name}page=1&key=${process.env.PERENUAL_API_KEY}`, axiosOption);
-      return plants.data;
-    } catch (err: unknown) {
-      console.log('Could not get plant list.');
-      return [];
-    }
+export async function getPlantList (name: string) {
+  try {
+    const plants = await axios.get(`${baseURL}species-list?q=${name}&page=1&key=${PERENUAL_API_KEY}`, axiosOption);
+    return plants.data;
+  } catch (err: unknown) {
+    console.log('Could not get plant list.', err);
+    return [];
+  }
+};
 
-  },
-  getPlantDetails: async (id: number) => {
-    try {
-      const plantDetails = await axios.get(`${baseURL}species/details/${id}?key=${process.env.PERENUAL_API_KEY}`);
-      return plantDetails.data;
-    } catch (err: unknown) {
-      console.log('Could not get plant details.');
-      return {};
-    }
-  },
-}
+export async function getPlantDetails (id: number) {
+  try {
+    const plantDetails = await axios.get(`${baseURL}species/details/${id}?key=${process.env.PERENUAL_API_KEY}`);
+    return plantDetails.data;
+  } catch (err: unknown) {
+    console.log('Could not get plant details.');
+    return {};
+  }
+};
