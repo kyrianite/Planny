abe2ac243060c095cd84ec3913edc6db2548d847
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-03-06 16:41:41 -0800

    working room implementation. text input resizing correclty

--- src/components/messages/chatroom.tsx
+++ src/components/messages/chatroom.tsx
@@ -1,9 +1,10 @@
 import React, { Component, useEffect, useState } from 'react';
-import { StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, TextInput, View, ScrollView, Image } from 'react-native';
 import io from 'socket.io-client';
 import {AutoGrowingTextInput} from 'react-native-autogrow-textinput';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { SafeAreaView } from 'react-native-safe-area-context';
+const placeholder1 = require('./images/placeholder-1.jpeg')
 
 export default function Chatroom() {
   const [messages, setMessages] = useState([]);
@@ -11,12 +12,14 @@ export default function Chatroom() {
   const [socket, setSocket] = useState(null);
   const [room, setRoom] = useState(null);
   const [user, setUser] = useState(null);
+  const [height, setHeight] = useState(41);
 
   const timeOptions = { weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', };
   var room123 = "Mom\'s House"; // this ref will be pulled from DB
   var name123 = "steve" //ref to be pulled from DB
 
   useEffect(() => {
+    console.log(height)
     setRoom(room123) // This is where we will set the room from MongoDB
     setUser(name123)
     const socket = io('http://localhost:3420');
@@ -31,27 +34,37 @@ export default function Chatroom() {
   const handleSendMessage = () => {
     socket.emit('message', {name: user, message: message, timeStamp: new Date().toLocaleString(undefined, timeOptions)});
     setMessage('');
+    setHeight(41)
+  };
+
+  const onContentSizeChange = (event) => {
+    setHeight(event.nativeEvent.contentSize.height);
   };
 
   return (
     <SafeAreaView style={styles.outsideContainer}>
      <View style={styles.container}>
+      <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: 5, marginLeft: 15, marginRight: 15}}>
+      <Image source={placeholder1} style={{width: 50, height: 50, borderRadius: 25}} />
+      <Image source={placeholder1} style={{width: 50, height: 50, borderRadius: 25}} />
+      <Image source={placeholder1} style={{width: 50, height: 50, borderRadius: 25}} />
+      </View>
       <ScrollView style={styles.messagesContainer}>
         {messages.map((message, index) => (
-          <View key={index * 10} style={{maxWidth: '100%'}}>
-            <Text style={{fontSize: 9, marginBottom: 10}} key={message.timeStamp}>{message.timeStamp}</Text>
+          <View key={index * 10} style={{maxWidth: '100%', marginBottom: 10}}>
+            <Text style={{fontSize: 9}} key={message.timeStamp}>{message.timeStamp}</Text>
             <Text style={{fontSize: 14, fontWeight: 'bold', maxWidth: '105%'}} key={index}>{message.name}:<Text style={{fontSize: 12, fontWeight: 'normal', marginLeft: 5, marginRight: 5}} key={message.message}>{message.message}</Text></Text>
           </View>
         ))}
       </ScrollView>
     </View>
       <View style={styles.inputContainer}>
-        <AutoGrowingTextInput
-          style={styles.input}
+        <TextInput
+          multiline
+          style={[styles.input, {height}, {overflow: 'hidden'}]}
           value={message}
           onChangeText={(text) => setMessage(text)}
-          maxHeight={200}
-          minHeight={45}
+          onContentSizeChange={(e) => onContentSizeChange(e)}
         />
         <Text style={styles.sendButton} onPress={handleSendMessage}>
           Send
@@ -104,7 +117,7 @@ export default function Chatroom() {
 const styles = StyleSheet.create({
   outsideContainer: {
     flex: 1,
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#C6D5BE',
   },
   container: {
     flex: 0.8,
