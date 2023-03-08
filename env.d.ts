8605cb6faa24da14862d9f05918d66e98fc263f8
Sandy Chu <sandy.chu.40@gmail.com>
2023-03-07 23:14:50 -0800

    Update env.d.ts
    Changed contents of env.d.ts files

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,10 @@
 declare module '@env' {
-  export const PERENUAL_API_KEY: "sk-wYTr6403c1e7656c0164";
-  export const PORT: 3000;
-  export const REACT_APP_FIREBASE_API_KEY: "AIzaSyCmMKQr48IO1WVpU9SVN5O7usmMttB5DnM";
-  export const REACT_APP_FIREBASE_AUTH_DOMAIN: "planny-test.firebaseapp.com";
-  export const REACT_APP_FIREBASE_PROJECT_ID: "planny-test";
-  export const REACT_APP_FIREBASE_STORAGE_BUCKET: "planny-test.appspot.com";
-  export const REACT_APP_FIREBASE_SENDER_ID: "258228663414";
-  export const REACT_APP_FIREBASE_APP_ID: "1:258228663414:web:1ad747c172d21c38375b57";
+  export const PERENUAL_API_KEY: string;
+  export const PORT: number;
+  export const REACT_APP_FIREBASE_API_KEY: string;
+  export const REACT_APP_FIREBASE_AUTH_DOMAIN: string;
+  export const REACT_APP_FIREBASE_PROJECT_ID: string;
+  export const REACT_APP_FIREBASE_STORAGE_BUCKET: string;
+  export const REACT_APP_FIREBASE_SENDER_ID: string;
+  export const REACT_APP_FIREBASE_APP_ID: string;
 }
