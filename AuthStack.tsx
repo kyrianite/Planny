94faa93e54037d5324407dd4e04e59af17de75cc
William <wwong.w3@gmail.com>
2023-03-09 12:08:24 -0600

    login screen complete

--- AuthStack.tsx
+++ AuthStack.tsx
@@ -1,14 +1,14 @@
-
-
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SignUpScreen from './src/screens/SignUp';
-import LoginScreen from './src/screens/Login'
+import LoginScreen from './src/screens/Login';
+import ForgotPasswordScreen from './src/screens/ForgotPassword';
 
 type AuthStackParamList = {
   Login: undefined;
   SignUp: undefined;
+  ForgotPassword: undefined;
 };
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
@@ -17,6 +17,7 @@ export default function AuthStack() {
     <Stack.Navigator>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
     </Stack.Navigator>
   );
 }
