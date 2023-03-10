b81d7cabc45edb7cb904bc90bb0fd554d63b7b16
Sandy Chu <sandy.chu.40@gmail.com>
2023-03-10 12:44:09 -0800

    Committing changes before pulling

--- src/screens/HouseGroup.tsx
+++ src/screens/HouseGroup.tsx
@@ -57,7 +57,6 @@ export default function HouseGroupScreen({navigation, route}) {
     makePlants();
   }, [])
 
-  // const props = route.params.p;
   function plantTouch() {
     if (loading) {
       return (
